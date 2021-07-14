0dd345f5f0adb586745d4d1088cb096a50e99adc
Maria Bruner <mariab2898@gmail.com>
2021-07-14 19:52:55 -0400

    setting up home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return(
     <div className='App'>
-      test
+      
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 const Home = () => {
     <div className='main'>
         <h1>Home page</h1>
+        Welcome to [Our Project Name!]
     </div>
 }
 
