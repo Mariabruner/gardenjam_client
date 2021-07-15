1df7d494c09c147e51e8d63f4a764a0d4da06fe1
Rhinicorn <admin@rhinicorn.com>
2021-07-15 19:41:47 -0400

    aw: footer addition

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
+import Footer from './components/Footer'
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
+
 function App() {
   return(
     <div className='App'>
+      <Router>
+      </Router>
       develop
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default App;
