09cc3d445f127aab0e3853b5780d14b9b93b5d32
Maria Bruner <mariab2898@gmail.com>
2021-07-15 19:10:24 -0400

    Added buttons to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import {Link} from 'react-router-dom'
+import {
+    Button
+} from 'reactstrap'
 
 const Home = () => {
     <div className='main'>
         <h1>Home page</h1>
         Welcome to [Our Project Name!]
+        <Button>Look up National Parks!</Button>
+        <Button>Go to the National Park forum!</Button>
     </div>
 }
 
