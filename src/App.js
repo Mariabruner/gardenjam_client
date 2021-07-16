d958e9bd8aa6e708971ae3f8e07a3e2c16d8de97
Katherine Fancher-Johnes <kljohnes@gmail.com>
2021-07-15 20:37:17 -0400

    kj adjustments to nav

--- src/App.js
+++ src/App.js
@@ -36,22 +36,13 @@ const protectedViews = () => {
 }
 
   return(
-<<<<<<< HEAD
-    <div className='App'>
-      <Router>
-      </Router>
-      develop
+    <div>
+      <Sitebar clickLogout={clearToken}/>
+      {protectedViews()}
+    
       <Footer />
     </div>
   );
 };
-=======
-    <div>
-      <Sitebar clickLogout={clearToken}/>
-      {protectedViews()}
-    </div>
-  );
-}
->>>>>>> 775b95586f47d5361df23230bbcb0d2215315b0b
 
 export default App;
