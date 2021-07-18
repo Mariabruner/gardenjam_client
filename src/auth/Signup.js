b100b138168cc8d60f11bb7bb0e937870079c22b
Katherine Fancher-Johnes <kljohnes@gmail.com>
2021-07-18 00:27:18 -0400

    kj added form validation to signup and updated styling on navbar

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,42 +1,112 @@
-  
-import React, {useState} from 'react';
-import {Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
+import React, { useState } from 'react';
+import { Form, FormGroup, Label, Input, Button, FormFeedback } from 'reactstrap';
 
 const Signup = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+
+    const [usernameValid, setUsernameValid] = useState(false);
+    const [passwordValid, setPasswordValid] = useState(false);
+    const [confirmPasswordValid, setConfirmPasswordValid] = useState(false);
+
+  
+
+    const formWrapper = {
+        border: '1px solid black',
+        borderRadius: '1em',
+        padding: '10px'
+    }
 
     let handleSubmit = (event) => {
         event.preventDefault();
         fetch("http://localhost:3000/user/register", {
             method: 'POST',
-            body: JSON.stringify({user:{username: username, password: password}}),
+            body: JSON.stringify({ user: { username: username, password: password } }),
             headers: new Headers({
                 'Content-Type': 'application/json'
             })
         }).then(
             (response) => response.json()
-            ).then((data) => {
-                props.updateToken(data.sessionToken)
-            })
+        ).then((data) => {
+            props.updateToken(data.sessionToken)
+        })
     }
 
-    return(
-        <div>
-        <h1>Sign Up</h1>
-        <Form onSubmit={handleSubmit}>
-            <FormGroup>
-                <Label htmlFor="username">Username</Label>
-                <Input onChange={(e) => setUsername(e.target.value)} name="username" value={username} />
+    return (
+        <div style={formWrapper} id='formWrapper'>
+            <h1>Sign Up</h1>
+            <Form onSubmit={handleSubmit}>
+                <FormGroup>
+                    <Label htmlFor="username">Username</Label>
+                    <Input valid={usernameValid} invalid={!usernameValid} onChange={(e) => {
+                        setUsername(e.target.value)
+                        if (e.target.value.length >= 4 && (e.target.value.includes ("@") || e.target.value.includes ("#") || e.target.value.includes ("$") || e.target.value.includes ("!") || e.target.value.includes ("%") || e.target.value.includes ("^") || e.target.value.includes ("*") || e.target.value.includes ("&")))
+                             {
+                            setUsernameValid(true)
+                        } else {
+                            setUsernameValid(false)
+                        }
 
 
-            </FormGroup>
-            <FormGroup>
-                <Label htmlFor="password">Password</Label>
-                <Input onChange={(e) => setPassword(e.target.value)} name="password" value={password}/>
-            </FormGroup>
-            <Button type="submit">Sign Up</Button>
-        </Form>
+                    }} name="username" value={username} />
+                    <FormFeedback invalid>
+                        {'Username must be at least 4 characters and include a special character (@, #, $, %, ^, &, *, !)'}
+                    </FormFeedback>
+
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="password">Password</Label>
+                    <Input valid={passwordValid} invalid={!passwordValid} onChange={(e) => {
+                        setPassword(e.target.value)
+                        if (e.target.value.length >= 5) {
+                            setPasswordValid(true);
+                        } else {
+                            setPasswordValid(false);
+                        }
+                        if (confirmPassword !== e.target.value || confirmPassword.length === 0) {
+                            setConfirmPasswordValid(false);
+                        } else {
+                            setConfirmPasswordValid(true);
+                        }
+
+                    }} name="password" value={password} />
+                    <FormFeedback invalid>
+                        {'Password must be at least 5 characters'}
+                    </FormFeedback>
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="confirmPassword">Confirm Password</Label>
+                    <Input invalid={!confirmPasswordValid} valid={confirmPasswordValid} onChange={(e) => {
+                        setConfirmPassword(e.target.value)
+                        if (password === e.target.value && e.target.value.length !== 0) {
+                            setConfirmPasswordValid(true);
+                        } else if (confirmPassword !== password) {
+                            setConfirmPasswordValid(false);
+                        } else {
+                            setConfirmPasswordValid(false);
+                        }
+                    }} name="confirmPassword" value={confirmPassword} /> 
+                    { <FormFeedback>
+                        {confirmPassword.length === 0
+                            ?
+                            'Please confirm your password'
+                            :
+                            confirmPassword !== password
+                                ?
+                                'Passwords do not match'
+                                :
+                                null}
+                    </FormFeedback> }
+
+
+
+         </FormGroup>
+
+                <Button type="submit" disabled={usernameValid && passwordValid && confirmPasswordValid ? false : true}>Sign Up</Button>
+            </Form>
         </div>
     )
 }
