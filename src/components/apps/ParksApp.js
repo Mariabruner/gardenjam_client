58e4aa0d136dbbf199cec53b9b98cdb460f67f91
Rhinicorn <admin@rhinicorn.com>
2021-07-20 19:49:23 -0400

    aw: api image search field/fetch

--- src/components/apps/ParksApp.js
+++ src/components/apps/ParksApp.js
@@ -1,24 +1,23 @@
 import React, {useState} from 'react';
+import ParkAppDisplay from './ParkAppDisplay/ParkAppDisplay'
 
 const ParksApp = () => {
   const [result, setResult] = useState();
   const [query, setQuery] = useState('');
   
   const fetcher = () => {
-    fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=ecZanu5QLHmV2m4qfjdv2Qxyv3duOJ7mnwz2whSj')
+    fetch('https://developer.nps.gov/api/v1/parks?api_key=ecZanu5QLHmV2m4qfjdv2Qxyv3duOJ7mnwz2whSj')
       .then(res => {
-        if(res.statis != 200){
+        if(res.status !== 200){
           throw new Error('fetch error');
         } else return res.json();
       })
-      .then(json => {
-        if(json.results.length ===0){
-          throw new Error('no results');
-        } else {
+      .then(json => { 
+
           const parkNum = Math.floor(Math.random()*json.results.length);
           setResult(json.results[parkNum]);
           console.log(json.results[parkNum])
-        }
+        
       })
       .catch(err => console.log(err))
   }
@@ -27,6 +26,7 @@ const ParksApp = () => {
       <div>
         <input value={query} onChange={(e) => setQuery(e.target.value)} />
         <button onClick={fetcher}>Click for the Park Picture Search</button>
+        {!result || !result.images.url ? null : <ParkAppDisplay park={result} />}
       </div>
     </div>
   )
