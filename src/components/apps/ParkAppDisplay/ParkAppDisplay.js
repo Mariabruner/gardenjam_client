7e03d745fa6a48ee98e02c38cbfaf5d40b1bbbcb
Katherine Fancher-Johnes <kljohnes@gmail.com>
2021-07-27 00:17:11 -0400

    kj updated styling on login/signup and park search page, added info to park search display and made images clickable links

--- src/components/apps/ParkAppDisplay/ParkAppDisplay.js
+++ src/components/apps/ParkAppDisplay/ParkAppDisplay.js
@@ -5,18 +5,27 @@ import 'bootstrap/dist/css/bootstrap.css'
 const ParkAppDisplay = (props) => {
   console.log(props)
 
+
   return (
     <div className="parkList" width="50%">
       {props.park.map((item, idx) =>
         <Media className="listItem">
-          <Media left href="#">
-            <Media object src={item.images[0].url} alt="Generic placeholder image" width="25%"/>
+          <Media left href={item.url}>
+            <Media object src={item.images[0].url} alt="Generic placeholder image" width="40%" id="parkPhoto"/>
           </Media>
           <Media body>
             <Media heading>
-              {item.fullName}
+              {item.fullName} - {item.addresses[0].city}, {item.addresses[0].stateCode}
             </Media>
+            <div>
             {item.description}
+            <br></br>
+            <h6>Weather Info:</h6>
+            {item.weatherInfo}
+            <br></br>
+            <h6>Operating Hours:</h6>
+            {item.operatingHours[0].description}
+            </div>
           </Media>
         </Media>
       )}
